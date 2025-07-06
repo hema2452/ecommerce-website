d5a8f9f89f676ee24000521405c73df4cfedd777
hema2452 <hemarachakonda19985@gmail.com>
2025-07-06 13:49:58 +0530

    Add files via upload
    Navbar was done

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react'
+import { NavLink,Link } from 'react-router-dom'
+import {assets} from "../assets/frontend_assets/assets"
+import '../css/NavBar.css'
+
+const NavBar = () => {
+  const [visible,setVisible] = useState(false)
+  return (
+    <div className = "navbar">
+      <Link to="/">
+              <img src={assets.logo} alt="Logo" className='logo'/>
+      </Link>
+      
+
+        <ul className='menu-items'>
+            <NavLink className="list-item" to="/">Home</NavLink>
+            <NavLink className="list-item" to="/collection">Collections</NavLink>
+            <NavLink className="list-item" to="/about">About</NavLink>            
+            <NavLink className="list-item" to="/contact">Contact</NavLink>
+        </ul>
+        {/*css property to add --- for small screen it is hidden about sm(small screens) it should be flex */}
+        <div className='icons-list'>
+          <img src={assets.search_icon} alt="search-icon"  className='icon'/>
+          <div className='profile-container'>
+            <img src={assets.profile_icon} alt="profile-icon" className='profile-image icon'/>
+            <div className="profile-icon">
+              <p>My Profile</p>
+              <p>Orders</p>
+              <p>Logout</p>
+            </div>
+          </div>
+          <Link to="/cart">
+              <img src={assets.cart_icon} alt="cart-icon" className='icon' />
+          </Link>
+          <img onClick={ () => setVisible(true)} src={assets.menu_icon} className='menu-icon icon'/>               
+        </div>
+        <div className={`${visible ? "small-screen-navbar" : "no-small-screen-navbar" }`}>
+          <div onClick ={() => setVisible(false)} className='toggle-icon'>
+            <img src={assets.cross_icon}/>
+            <p>Back</p>
+          </div>
+            <NavLink onClick={()=>setVisible(false)} className="toggle-item" to="/">Home</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className="toggle-item" to="/collection">Collections</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className="toggle-item" to="/about">About</NavLink>            
+            <NavLink onClick={()=>setVisible(false)} className="toggle-item" to="/contact">Contact</NavLink>
+
+
+        </div>
+                      
+    </div>
+  )
+}
+
+export default NavBar
